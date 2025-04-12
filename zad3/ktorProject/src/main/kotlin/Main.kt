1b97df4023cccb0745c58bc2444c0135ffd11601
kasiakowalczyk <kasia29a@gmail.com>
2025-04-12 10:51:04 +0200

    4.5

--- zad3/ktorProject/src/main/kotlin/Main.kt
+++ zad3/ktorProject/src/main/kotlin/Main.kt
@@ -10,6 +10,7 @@ import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.*
 import kotlinx.serialization.json.*
+import java.util.*
 
 const val botToken = "MTM2MDUyMzQ1MjgzODMxNDExNA.GXzWix.k7BMpmBex-qs_d5kvPs184wydO4Hzf7ZW1bkf0"
 const val channelId = "1360527507891290192"
@@ -36,10 +37,16 @@ suspend fun runBot() {
 
     bot.on<MessageCreateEvent> {
         if (message.author?.isBot == false) {
+            val foundCategory = CATEGORIES.find { message.content.contains(it, ignoreCase = true) }
             when {
                 message.content.contains(CATEGORIES_KEY) -> {
                     getCategories(message)
                 }
+
+                foundCategory != null -> {
+                    getProducts(message, foundCategory)
+                }
+
                 else -> {
                     respond(message, "Thank you for messaging me!")
                 }
@@ -56,6 +63,17 @@ suspend fun runBot() {
 private suspend fun getCategories(message: Message) {
     val response = CATEGORIES.joinToString(separator = "\n") { "- $it" }
     respond(message, "Available categories:\n$response")
+}
+
+private suspend fun getProducts(message: Message, category: String) {
+    val productList =
+        PRODUCTS[category.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }]
+    if (productList != null) {
+        val response = productList.joinToString(separator = "\n") { "- $it" }
+        respond(message, "Available products:\n$response")
+    } else {
+        respond(message, "There is no products available!")
+    }
 }
 
 private suspend fun respond(message: Message, response: String) {
--- zad3/ktorProject/src/main/kotlin/consts.kt
+++ zad3/ktorProject/src/main/kotlin/consts.kt
@@ -1,3 +1,13 @@
-val CATEGORIES = listOf("Elektronika", "Moda", "Dom i ogród", "Sport", "Książki")
+val CATEGORIES = listOf("Electronics", "Fashion", "Home and Garden", "Sports", "Books")
 
 const val CATEGORIES_KEY = "categories"
+
+const val PRODUCTS_KEY = "products"
+
+val PRODUCTS = mapOf(
+    "Electronics" to listOf("Laptop", "Phone", "Headphones"),
+    "Fashion" to listOf("Shirt", "Pants", "Coat"),
+    "Home and Garden" to listOf("Table", "Chair", "Lamp"),
+    "Sports" to listOf("Ball", "Bicycle", "Tennis Racket"),
+    "Books" to listOf("Thriller", "Crime", "Science Fiction")
+)
